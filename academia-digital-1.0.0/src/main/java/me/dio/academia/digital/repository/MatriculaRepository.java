61d556ab99eb32798e0dcebe120774d6a554a616
RiltonMonte <rilton_21@hotmail.com>
2022-05-31 12:05:26 -0300

    Primeiro commit

---
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -0,0 +1,13 @@
+package me.dio.academia.digital.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import me.dio.academia.digital.entity.Matricula;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long>{
+
+    List<Matricula> findByAlunoBairro(String bairro);
+}
